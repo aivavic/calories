d3024af0f009f58e84ab0f071bdf03581a292d88
Anna Shugai <anya.schugay@gmail.com>
2018-07-19 19:21:47 +0300

    her ego znaet

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -101,4 +101,11 @@ class ProductController extends Controller
         return $this->redirectToRoute('product');
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/registration", name="registration")
+     */
+    public function registration() {
+        return $this->render('product/registration.html.twig', []);
+    }
 }
